98f944110affda21b183348349de619b432658a8
Geoffrey Gao <geoffreygao1@gmail.com>
2023-02-28 19:07:09 -0800

    Added AnimalsController

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -4,8 +4,8 @@ namespace AnimalShelter.Models
   {
     public int AnimalId { get; set; }
     public string Type { get; set; }
+    public string Breed { get; set; }
     public string Name { get; set; }
     public string DateAdmitted { get; set; }
-    public string Breed { get; set; }
   }
 }
