585924ee1f8f25fd260d2693195b1c209b2759f6
Geoffrey Gao <geoffreygao1@gmail.com>
2023-02-28 19:33:33 -0800

    Added details route

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -32,5 +32,10 @@ namespace AnimalShelter.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Details(int id)
+    {
+      Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+      return View(thisAnimal);
+    }
   }
 }
